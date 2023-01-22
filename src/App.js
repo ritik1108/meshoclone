00bc65ed19490c6806cb649a4beb79e206b40566
Akash Gadekar <91712982+ARG24@users.noreply.github.com>
2023-01-22 23:25:56 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+// import Address from "./Components/Address/Address";
+// import Checkout from "./Components/Checkout/Checkout";
+
+import Home from "./Componant/Home/Home";
+// import Login from "./Components/Login/Login";
+// import Payment from "./Components/Payment/Paynment";
+// import SignUp from "./Components/SignUp/Signup";
+
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
+// import AddProduct from "./Components/AddProduct/AddProduct";
+// import Orders from "./Components/Orders/Orders";
+
+// const promise = loadStripe(
+//   "pk_test_51KUDBXSE1AGsrDtwyXK8vcHYNkEOofJAP1vV1fRlpZNo93g4o80dZe4IvhAkBXo2ytDciCqqpynwQUXv7plCjezF00G9zyj4sc"
+// );
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/address" element={<Address />} /> */}
+          {/* <Route
+            path="/payment"
+            element={
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>
+            }
+          /> */}
+
+          {/* <Route path="/addproduct" element={<AddProduct />} />
+          <Route path="/orders" element={<Orders />} /> */}
+        </Routes>
+      </Container>
+    </Router>
   );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 export default App;
